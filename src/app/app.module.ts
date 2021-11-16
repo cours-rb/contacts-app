f345a860125030a05f2c897c1c0d89d3606162e9
Renaud BOYER <rboyer@linagora.com>
2021-11-16 14:24:49 +0100

    Ajoute un formulaire contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts.component';
 import { ContactComponent } from './contact.component';
 import { ContactDetailComponent } from './contact-detail.component';
+import { ContactFormComponent } from './contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
     ContactComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule
